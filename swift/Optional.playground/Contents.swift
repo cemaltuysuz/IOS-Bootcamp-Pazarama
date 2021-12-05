e6f27786d2a83b05c5317044ce82f450bd6f5a6a
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-12-05 15:13:50 +0300

    Optional

---
+++ swift/Optional.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var str:String? = nil
+var str1:String
+
+str = "Hello !"
+
+if let temp = str { // optional binding
+    print(temp)
+}else{
+    print("Str is nil")
+}
+
+
+
+
+
+
