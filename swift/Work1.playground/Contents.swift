d8a94d35430215f6062885c672c04a775990fd85
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-12-06 18:42:49 +0300

    Work 1

---
+++ swift/Work1.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+
+class Islem {
+    var islemNo:Int?
+    var ogrNo:Int?
+    var kitapNo:Int?
+    var aTarih:Date?
+    var vTarih:Date?
+}
+
+class Ogrenci {
+    var ogrNo:Int?
+    var ogrAd:String?
+    var cinsiyet:String?
+    var dTarih:Date?
+    var sinif:String?
+}
+
+class Yazar {
+    var yazarNo:Int?
+    var yazarAd:String?
+    var yazarSoyad:String?
+}
+
+class Kitap {
+    var kitapNo:Int?
+    var isbnNo:Int?
+    var kitapAdi:String?
+    var yazarNo:Int?
+    var turNo:Int?
+}
+
+class tur {
+    var turNo:Int?
+    var turAdi:String?
+}
+
