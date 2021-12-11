5f8c31ebe263a30651f3799ab715bfb73bf1665e
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-12-11 13:34:58 +0300

    OOP 2 | 1

--- swift/ObjectOriented2.playground/Contents.swift
+++ swift/ObjectOriented2.playground/Contents.swift
@@ -1,3 +1,35 @@
 import UIKit
 
-var str = "Hello, playground"
+class Home{
+    var windowCount:Int?
+    init(windowCount:Int) {
+        self.windowCount = windowCount
+    }
+}
+
+class Palace : Home{
+    var towerCount:Int?
+    init(towerCount:Int,windowCount:Int) {
+        self.towerCount = towerCount
+        super.init(windowCount: windowCount)
+    }
+}
+
+class Villa : Home{
+    var garage:Bool?
+    init(garage:Bool,windowCount:Int) {
+        self.garage = garage
+        super.init(windowCount: windowCount)
+    }
+}
+
+var topkapiPalace = Palace(towerCount: 30, windowCount: 100)
+var villa = Villa(garage: true, windowCount: 30)
+
+print(topkapiPalace.towerCount!)
+print(topkapiPalace.windowCount!)
+
+print(villa.garage!)
+print(villa.windowCount!)
+
+
