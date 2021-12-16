0154e533ec22b486a0f4264046850b056b3be5bc
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-12-16 21:23:13 +0300

    work3 | persons

--- ios/Work3/Work3/ViewController.swift
+++ ios/Work3/Work3/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var currencyUnit: UIView!
     @IBOutlet weak var assetInfo: UIView!
+    @IBOutlet weak var person: UIImageView!
+    @IBOutlet weak var person2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +25,14 @@ class ViewController: UIViewController {
         assetInfo.layer.borderWidth = 0.5
         assetInfo.layer.borderColor = UIColor(named: "LightGray")!.cgColor
         assetInfo.layer.cornerRadius = 13
+        
+        // Person
+        person.layer.masksToBounds = true
+        person.layer.cornerRadius = person.bounds.width / 2
+        
+        // Person 2
+        person2.layer.masksToBounds = true
+        person2.layer.cornerRadius = person2.bounds.width / 2
     }
 
 
