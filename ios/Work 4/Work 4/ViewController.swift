d154ab1b16ee2ddcefd3275f35e78391a903c23d
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-12-17 23:10:04 +0300

    work 4 | Created screens

---
+++ ios/Work 4/Work 4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Work 4
+//
+//  Created by cemal tüysüz on 17.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
