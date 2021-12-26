4bd7f6e0e078049dba0f8ba9a1ec71ccc008cd57
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-12-26 19:28:04 +0300

    Work 6 | init, inserted product images, model etc.

---
+++ ios/Work 6/Work 6/Product.swift
@@ -0,0 +1,18 @@
+//
+//  Product.swift
+//  Work 6
+//
+//  Created by cemal tüysüz on 26.12.2021.
+//
+
+import Foundation
+
+class Product {
+    var productId:Int?
+    var productImage:String?
+    var productTitle:String?
+    var productDescription:String?
+    var productPrice:Double?
+    var isShippingFree:Bool?
+    var isFastDelivery:Bool
+}
