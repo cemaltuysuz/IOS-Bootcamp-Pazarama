5c600dd34186b0be62a7428024a447ee2a621686
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-12-30 16:53:06 +0300

    Work 7 | init, design, widget binding, VIPER folders, tableview custom cell

---
+++ ios/Work 7/Work 7/home-module/tableview/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  Work 7
+//
+//  Created by cemal tüysüz on 30.12.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ios/Work 7/Work 7UITests/Work_7UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Work_7UITestsLaunchTests.swift
+//  Work 7UITests
+//
+//  Created by cemal tüysüz on 30.12.2021.
+//
+
+import XCTest
+
+class Work_7UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
