53c1e99b7ab8bfac225c1f8db001f867fd9bafe2
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2022-01-05 20:43:19 +0300

    Kisiler Uygulaması URLSeission | delete, search

--- ios/KisilerUygulamasiURL/KisilerUygulamasi/anasayfa-module/interactor/AnasayfaInteractor.swift
+++ ios/KisilerUygulamasiURL/KisilerUygulamasi/anasayfa-module/interactor/AnasayfaInteractor.swift
@@ -34,10 +34,49 @@ class AnasayfaInteractor : PresenterToInteractorAnasayfaProtocol {
     }
     
     func kisiAra(aramaKelimesi: String) {
-        print("Arama sonucu : \(aramaKelimesi)")
+        var istek = URLRequest(url: URL(string: "http://kasimadalan.pe.hu/kisiler/tum_kisiler_arama.php")!)
+        istek.httpMethod = "POST"
+        let postString = "kisi_ad=\(aramaKelimesi)"
+        istek.httpBody = postString.data(using: .utf8)
+        
+        URLSession.shared.dataTask(with: istek) {(data,response,error) in
+            if error != nil && data == nil {
+                print("hata")
+                return
+            }
+            do{
+                let cevap = try JSONDecoder().decode(KisilerCevap.self, from: data!)
+                
+                var liste = [Kisiler]()
+                if let gelenListe = cevap.kisiler {
+                    liste = gelenListe
+                }
+                self.anasayfaPresenter?.presenteraVeriGonder(kisilerListesi: liste)
+            }catch{
+                print(error.localizedDescription)
+            }
+        }.resume()
     }
     
     func kisiSil(kisi_id: Int) {
-        print("\(kisi_id) silindi")
+        var istek = URLRequest(url: URL(string: "http://kasimadalan.pe.hu/kisiler/delete_kisiler.php")!)
+        istek.httpMethod = "POST"
+        let postString = "kisi_id=\(kisi_id)"
+        istek.httpBody = postString.data(using: .utf8)
+        
+        URLSession.shared.dataTask(with: istek) {(data,response,error) in
+            if error != nil && data == nil {
+                print("hata")
+                return
+            }
+            do{
+                if let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String:Any] {
+                    print(json)
+                }
+            }catch{
+                print(error.localizedDescription)
+            }
+        }.resume()
+        tumKisileriAl()
     }
 }
